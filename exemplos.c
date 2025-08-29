0b82b5999d6ab4686f3c96e58c9c7a0b6388ce67
Programarxdj <alexandre.franca242@gmail.com>
2025-08-29 03:15:36 +0000

    strutura_de_decisaosimples

---
+++ exemplos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+ int main(){
+
+    int numero = 4, resultado;
+
+    resultado = numero % 2;
+
+        printf("a variavel resultado é: %d\n", resultado);
+
+       if(resultado == 0) {
+            printf("o numero é par\n");
+       }
+}
